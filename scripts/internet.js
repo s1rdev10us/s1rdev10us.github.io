c456f64a3670bb77872b5a35eee1f77891893791
Sir Devi0us <S1rDev10us@outlook.com>
2021-12-16 20:45:03 +0000

    at this point i just cant anymore

---
+++ scripts/internet.js
@@ -0,0 +1,17 @@
+let myVar = setInterval(myTimer, 1000);
+var online = true;
+function myTimer() {
+	if (navigator.onLine != online) {
+		if (navigator.onLine == true) {
+			console.log('now online');
+			alert('now online');
+			online = navigator.onLine;
+		} else {
+			console.log('now offline');
+			alert('now offline');
+			online = navigator.onLine;
+        }
+	}
+	console.log(navigator.onLine);
+
+}
