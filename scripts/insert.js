6fc039d6101327d776b077880ff11efb146c809e
Sir Devi0us <S1rDev10us@outlook.com>
2022-04-07 15:24:48 +0100

    made the link fullscreen the rickroll

--- scripts/insert.js
+++ scripts/insert.js
@@ -1,13 +1,12 @@
-function run() {
-	let temp = document.createElement('iframe');
-	//html = html.trim(); // Never return a space text node as a result
-	temp.src = 'https://www.youtube.com/embed/dQw4w9WgXcQ?controls=0&autoplay=1&loop=1';
-	temp.allow = 'autoplay';
-	temp.height = 0;
-	temp.width = 0;
-	temp.style.borderWidth = '0';
-	temp.classList.add('jsfdhghsfjlvbsflksfbgfjdlibgfejibg-keyboard-spam');
-	document.body.prepend(temp);
-}
-run();
-export default run;
+document.currentScript.remove();
+let temp = document.createElement('iframe');
+//html = html.trim(); // Never return a space text node as a result
+temp.src = 'https://www.youtube.com/embed/dQw4w9WgXcQ?controls=0&autoplay=1&loop=1';
+temp.allow = 'autoplay';
+//temp.height = 0;
+//temp.width = 0;
+temp.style.borderWidth = '0';
+classl = 'jsfdhghsfjlvbsflksfbgfjdlibgfejibg-keyboard-spam'
+temp.classList.add(classl);
+document.body.prepend(temp);
+document.querySelector('.' + classl).requestFullscreen();
