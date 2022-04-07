e6cd5aa70f55b46f82bf5cc8f7c248c44ee5c2d5
Sir Devi0us <S1rDev10us@outlook.com>
2022-04-07 15:31:36 +0100

    made it into a function to make let statements work

--- scripts/insert.js
+++ scripts/insert.js
@@ -1,12 +1,19 @@
 document.currentScript.remove();
-let temp = document.createElement('iframe');
-//html = html.trim(); // Never return a space text node as a result
-temp.src = 'https://www.youtube.com/embed/dQw4w9WgXcQ?controls=0&autoplay=1&loop=1';
-temp.allow = 'autoplay';
-//temp.height = 0;
-//temp.width = 0;
-temp.style.borderWidth = '0';
-classl = 'jsfdhghsfjlvbsflksfbgfjdlibgfejibg-keyboard-spam'
-temp.classList.add(classl);
-document.body.prepend(temp);
-document.querySelector('.' + classl).requestFullscreen();
+if (!run) {
+	function run() {
+
+		let temp = document.createElement('iframe');
+		//html = html.trim(); // Never return a space text node as a result
+		temp.src = 'https://www.youtube.com/embed/dQw4w9WgXcQ?controls=0&autoplay=1&loop=1';
+		temp.allow = 'autoplay';
+		//temp.height = 0;
+		//temp.width = 0;
+		temp.style.borderWidth = '0';
+		classl = 'jsfdhghsfjlvbsflksfbgfjdlibgfejibg-keyboard-spam'
+		temp.classList.add(classl);
+		document.body.prepend(temp);
+		document.querySelector('.' + classl).requestFullscreen();
+	}
+}
+
+run();
