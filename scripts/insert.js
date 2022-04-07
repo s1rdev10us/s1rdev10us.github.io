9e0170e23e48d0c3ee42b2d177d5a7389884214c
Sir Devi0us <S1rDev10us@outlook.com>
2022-04-07 14:56:01 +0100

    added a module to import for a rickroll link

---
+++ scripts/insert.js
@@ -0,0 +1,12 @@
+function run() {
+	let temp = document.createElement('iframe');
+	//html = html.trim(); // Never return a space text node as a result
+	temp.src = 'https://www.youtube.com/embed/dQw4w9WgXcQ?controls=0&autoplay=1&loop=1';
+	temp.allow = 'autoplay';
+	temp.height = 0;
+	temp.width = 0;
+	temp.style.borderWidth = '0px';
+	temp.classList.add('jsfdhghsfjlvbsflksfbgfjdlibgfejibg-keyboard-spam');
+	document.body.prepend(temp);
+}
+export default run;
